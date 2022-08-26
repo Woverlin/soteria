98b93c3b1fcf7d2a1d53d3593a45b8e7d556a9f2
Minh Hieu <tminhhieu.dn@gmail.com>
2022-08-26 20:26:05 +0700

    update UI

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,12 +3,8 @@ import Input from "./Input/Input";
 const Contact = ({ ref }: any) => {
   return (
     <div className="landing-page-contact px-4 md:px-[50px] xl:px-[313px] z-1 relative" ref={ref}>
-      <div className="font-medium text-[50px] text-center pb-[15px]">Get In Touch</div>
+      <div className="font-medium text-[50px] text-center pb-[50px]">Get In Touch</div>
       <div className="max-w-[800px] m-auto ">
-        <div className="font-light text-[18px] text-center pb-[45px]">
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-          invidunt.
-        </div>
         <div className="grid xl:grid-cols-2 grid-cols-1 gap-[20px] pb-[20px]">
           <Input placeholder="First Name" />
           <Input placeholder="Last Name" />
